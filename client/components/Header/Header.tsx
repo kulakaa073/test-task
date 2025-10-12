b3ef072c595e33b01fa828ecb8ef50534a4f63ae
Shade <kulakaa073@gmail.com>
2025-10-12 20:03:35 +0300

    style: updatig header buttons style

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -14,20 +14,27 @@ export const Header = () => {
 
   return (
     <Navbar position="static">
-      <NavbarContent>
-        {routes.map((route) => {
-          const isActive = pathname === `/${route.route}`;
-          return (
-            <NavbarItem
-              isActive={isActive}
-              key={route.name + '_' + route.route}
-              className="data-[active=true]:font-bold"
-            >
-              <Link href={`/${route.route}`}>{route.route}</Link>
-            </NavbarItem>
-          );
-        })}
-      </NavbarContent>
+      <div className="p-[1px] bg-gradient-to-r from-[#FF1CF7] to-[#00F0FF] rounded-full mx-auto">
+        <NavbarContent className="flex gap-1 rounded-full overflow-hidden">
+          {routes.map((route) => {
+            const isActive = pathname === `/${route.route}`;
+            return (
+              <NavbarItem
+                isActive={isActive}
+                key={route.name + '_' + route.route}
+                className="data-[active=true]:font-bold bg-black py-2 px-6 group"
+              >
+                <Link
+                  href={`/${route.route}`}
+                  className="group-data-[active=true]:bg-gradient-to-r group-data-[active=true]:from-pink-500 group-data-[active=true]:to-cyan-500 group-data-[active=true]:text-transparent group-data-[active=true]:bg-clip-text"
+                >
+                  {route.route}
+                </Link>
+              </NavbarItem>
+            );
+          })}
+        </NavbarContent>
+      </div>
     </Navbar>
   );
 };
